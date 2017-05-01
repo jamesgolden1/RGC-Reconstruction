9488e7c3735cb76db98a4e5534565702bf46ae38
james golden <jamesgolden1@gmail.com>
2017-05-01 12:17:23 -0700

    updated build for current rgc format

--- @recon/build.m
+++ @recon/build.m
@@ -28,114 +28,86 @@ tic
 %% Parameters to alter
 
 % Retinal patch eccentricity
-% patchEccentricity = 12; % mm
+patchEccentricity = 1.8; % mm
 
 % Field of view/stimulus size
 % Set horizontal field of view
-fov = 2*1.6;
+fov = 3.2;
 
 % Stimulus length = nSteps*nBlocks;
+nPixels = 100;
 nSteps = 12000;
 nBlocks = 15;%30;
 
-%% Load image
-clear params
-% One frame of a moving bar stimulus
-% Set parameters for size
-params.nSteps = nSteps;
-params.row = 100;
-params.col = 100;
-params.fov = fov;
-% % params.vfov = 0.7;
-
-iStim = ieStimulusBinaryWhiteNoise(params);
-absorptions = iStim.sensor;
-whiteNoise = iStim;
-
-%% Outer segment calculation
-% There is no simulated outer segment, this identity outer segment acts as
-% a pass-through holder of the stimulus intensity information.
-
-% Input = RGB
-os = osCreate('displayrgb');
-
-sceneSize = sceneGet(whiteNoise.scene,'size');
-retinalPatchWidth = sensorGet(whiteNoise.sensor,'width','m');
-retinalPatchHeight = (sceneSize(1)/sceneSize(2))*retinalPatchWidth;
-
-os = osSet(os, 'patchSize', retinalPatchWidth);
-
-timeStep = (1/125)/1;
-os = osSet(os, 'timeStep', timeStep);
-
-os = osSet(os, 'rgbData', whiteNoise.sceneRGB);
-
-retinalPatchSize = osGet(os,'size');
-
-%% Build RGC array
-
-    paramsIR.name    = 'Macaque inner retina 1'; % This instance
-    paramsIR.eyeSide   = 'left';   % Which eye
-    paramsIR.eyeRadius = 1.8;        % Radius in mm
-    paramsIR.eyeAngle  = 90;       % Polar angle in degrees
-    
-    model   = 'LNP';    % Computational model
-    innerRetina = irCreate(os,paramsIR);
-    innerRetina = rgcMosaicCreate(innerRetina,'type','onParasol','model',model);
-    innerRetina = rgcMosaicCreate(innerRetina,'type','offParasol','model',model);
-    innerRetina = rgcMosaicCreate(innerRetina,'type','offMidget','model',model);
-    innerRetina = rgcMosaicCreate(innerRetina,'type','onMidget','model',model);
-    
-    % innerRetina = rgcMosaicCreate(innerRetina,'type','sbc','model',model);    
-    
-    mosaicFile = ['mosaicAll_' num2str(round(cputime))];
-    filenameRGC = [reconstructionRootPath '/dat/test/' mosaicFile '.mat'];
-    
-    innerRetina.mosaic{1}
-    innerRetina.mosaic{2}
-    innerRetina.mosaic{3}
-    innerRetina.mosaic{4}
-    save(filenameRGC, 'innerRetina');
-%     mosaicFile = 'mosaicAll_1246640';
-%     filenameRGC = [reconstructionRootPath '/dat/ns100_r2_10_regmos/' mosaicFile '.mat'];
-% else
-%     
-%     % filenameRGC = [reconstructionRootPath '\dat\mosaic_all.mat'];
-%     % filenameRGC = [reconstructionRootPath '/dat/mosaic_all_bertha_ns0.mat'];
-%     filenameRGC = [reconstructionRootPath '/dat/' mosaicFile '.mat'];
-% end
 
 for blockNum =1%:nBlocks
     tic
-    % clear psthNorm spikesout spikesoutM spikesoutsm whiteNoiseSmall whiteNoise iStim absorptions innerRetina
-    
     
     blockNum
-    %%% Grating subunit stimulus
-    load(filenameRGC, 'innerRetina');
-%     iStim = ieStimulusBinaryWhiteNoise(params);
-%     absorptions = iStim.sensor;
-%     whiteNoise = iStim;
+%     load(filenameRGC, 'innerRetina');
     
-    % % If training mosaics separately and need to load white noise movie
-    % filename1 = [reconstructionRootPath '\dat\WNstim_response_OnParasol_block_' num2str(blockNum) '.mat'];
-    % load(filename1); clear spikesoutsm;
-    % whiteNoise.sceneRGB = double(whiteNoiseSmall);
+    %      load([ reconstructionRootPath  '/dat/imagenetBlocks/movsm_' num2str(blockNum) '.mat'],'movsm');
+    load('/Volumes/Lab/Users/james/RGC-Reconstruction/dat/imagenetBlocks/movsm_1.mat','movsm');
+    %           load([ reconstructionRootPath  '\dat\imagenetBlocks\movsm_' num2str(blockNum) '.mat'],'movsm');
     
-    
-%      load([ reconstructionRootPath  '\dat\movsm_' num2str(blockNum) '.mat'],'movsm');
-
-%      load([ reconstructionRootPath  '/dat/imagenetBlocks/movsm_' num2str(blockNum) '.mat'],'movsm');
-          load('/Volumes/Lab/Users/james/RGC-Reconstruction/dat/imagenetBlocks/movsm_1.mat','movsm');
-%      load([ reconstructionRootPath  '/dat/imagenetBlocks/movsm_' num2str(blockNum) '.mat'],'movsm');
-%           load([ reconstructionRootPath  '\dat\imagenetBlocks\movsm_' num2str(blockNum) '.mat'],'movsm');
     natScenes = movsm(1:100,1:100,randperm(nSteps));
-    os = osSet(os, 'rgbData', double(natScenes));
     
-    innerRetina = irCompute(innerRetina,os);
+    %%
+    %% Load image
+    clear coneParams
     
-    % irPlot(innerRetina, 'linear');
-    % irPlot(innerRetina, 'psth');
+    % One frame of a WN stimulus
+    % Set parameters for size
+    
+    % coneParams.nSteps = nSteps;
+    % coneParams.row = 100; % should be set size to FOV
+    % coneParams.col = 100;
+    coneParams.fov = fov;
+    % % params.vfov = 0.7;
+    
+    
+    iStimNS = ieStimulusMovieCMosaic(rand(100,100,1),coneParams);
+    cMosaicNS = iStimNS.cMosaic;
+    
+    %% Bipolar
+    clear bpMosaic
+    
+    cellType = {'ondiffuse','offdiffuse','onmidget','offmidget','onSBC'};
+    for cellTypeInd = 1:4
+        clear bpParams
+        bpParams.cellType = cellType{cellTypeInd};
+        
+        bpParams.ecc = patchEccentricity;
+        bpParams.rectifyType = 1;
+        bpMosaic{cellTypeInd} = bipolar(cMosaicNS, bpParams);
+        bpMosaic{cellTypeInd}.set('sRFcenter',1);
+        bpMosaic{cellTypeInd}.set('sRFsurround',0);
+        bpMosaic{cellTypeInd}.compute(cMosaicNS);
+    end
+    
+    %% RGC
+    clear params rgcParams
+    params.eyeRadius = patchEccentricity;
+    params.eyeAngle = 90;
+    innerRetina=ir(bpMosaic,params);
+    cellType = {'on parasol','off parasol','on midget','off midget'};
+    
+    rgcParams.centerNoise = 0;
+    rgcParams.model = 'LNP';
+%     rgcParams.ellipseParams = [1 1 0];  % Principle, minor and theta
+    
+    rgcParams.type = cellType{1};
+    innerRetina.mosaicCreate(rgcParams);
+    rgcParams.type = cellType{2};
+    innerRetina.mosaicCreate(rgcParams);
+    rgcParams.type = cellType{3};
+    innerRetina.mosaicCreate(rgcParams);
+    rgcParams.type = cellType{4};
+    innerRetina.mosaicCreate(rgcParams);
+    
+%     innerRetina.compute(bpMosaic);
+    
+    %%
     toc
     %% Look at covariance matrix
     tic
@@ -153,14 +125,8 @@ for blockNum =1%:nBlocks
     spikesoutsm(size(spikesout,1)+size(spikesout2,1)+[1:size(spikesout3,1)] ,1:size(spikesout3,2) ) = spikesout3;
     spikesoutsm(size(spikesout,1)+size(spikesout2,1)+size(spikesout3,1)+[1:size(spikesout4,1)] ,1:size(spikesout4,2) ) = spikesout4;
     
-%     whiteNoiseSmall = uint8(squeeze(whiteNoise.sceneRGB(:,:,:,1)));
     whiteNoiseSmall = natScenes;
-    
-%     spikesoutsm = uint8(spikesoutmat);
-    
-    % filename1 = [reconstructionRootPath '\dat\NSstim_response_overlap0_block_' num2str(blockNum) '.mat'];
-    % filename1 = [reconstructionRootPath '/dat/nsResponses/NSstim_response_betha_ns0_block_' num2str(blockNum) '.mat'];
-%     filename1 = [reconstructionRootPath '/dat/nsResponses/' buildFile '_block_' num2str(blockNum) '.mat'];
+
     if ismac || isunix
         filename1 = [reconstructionRootPath '/dat/test/' buildFile '_block_' num2str(blockNum) '_' mosaicFile '.mat'];
     else
